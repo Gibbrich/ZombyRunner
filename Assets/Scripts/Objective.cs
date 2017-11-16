6d75d9a98a9ce8f4e5bf87eb2b67fc1770a2e41f
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2017-11-16 22:43:33 +0300

    Added objectives

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Objective : MonoBehaviour
+{
+    private static readonly string COMPLETED = "1/1";
+    
+    [SerializeField]
+    private Text state;
+
+    private Text objective;
+    
+    // Use this for initialization
+    void Start()
+    {
+        objective = GetComponent<Text>();
+    }
+
+    public void Show()
+    {
+        objective.enabled = true;
+        state.enabled = true;
+    }
+
+    public void Complete()
+    {
+        state.text = COMPLETED;
+    }
+}
